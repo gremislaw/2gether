95c585cdbd39cd96d4b29578ca220e44825eed73
yarsult <$@W$@w25-77047>
2023-09-09 00:22:29 +0300

    причесали чуть

--- main.py
+++ main.py
@@ -1,81 +1,3 @@
-# from telegram import ReplyKeyboardMarkup
-# from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
-import database_funcs
-from secret import TOKEN
-
-#
-# state = 0
-# reply_keyboard_main = [['Study buddy'], ['Чувак чтоб вместе пообедать'], ['Товарищ по интересам'], ['Земляк'], ['На другом языке']]
-# reply_keyboard_ege = [['Кто я', 'Задание'], ['Теория', 'Анекдот'], ['Команды', 'На главную']]
-# markup_main = ReplyKeyboardMarkup(reply_keyboard_main, one_time_keyboard=False)
-# markup_ege = ReplyKeyboardMarkup(reply_keyboard_ege, one_time_keyboard=False)
-# messages = dict(initial="Я могу помочь тебе с поиском друзей "
-#                         "Выбери, кого ты хочешь найти",
-#                 unclear_num="Я не понимаю, какой номер ты имел в виду... Попробуй еще раз")
-#
-#
-# def main():
-#     updater = Updater(TOKEN, use_context=True)
-#     dp = updater.dispatcher
-#     dp.add_handler(CommandHandler('start', start))
-#     dp.add_handler(MessageHandler(Filters.text, text))
-#     updater.start_polling()
-#     updater.idle()
-#
-#
-# def start(update, context):
-#     global id, markup_main
-#
-#
-#
-#     id = update.message.from_user.id
-#     if database_funcs.check_if_user_in_base(id) is None:
-#         update.message.reply_text('Привет! Круто, что ты поступил в МИСИС. Я могу помочь тебе освоиться')
-#         update.message.reply_text('Заполни, пожалуйства, небольшую анкету, она нужна, чтобы лучше помочь тебе')
-#         name(update)
-#
-#
-#         update.message.reply_text('Приятно познакомиться!\nНа каком ты курсе?')
-#         course = update.message.text
-#         update.message.reply_text('Отлично\nВ каком институте ты учишься?')
-#         institute = update.message.text
-#         database_funcs.add_user_to_base(id, name, institute, course)
-#     update.message.reply_text(messages['initial'], reply_markup=markup_main)
-#
-#
-# def name(update):
-#     global state
-#     update.message.reply_text('Твое имя?')
-#     name = update.message.text
-#     state = name
-#
-#
-# def text(update, context):
-#     global state, markup_ege
-#     req = update.message.text.lower()
-#     if state == name:
-#         update.message.reply_text('Идём дальше', reply_markup=markup_ege)
-#     if req == 'подготовка к егэ':
-#         start_ege(update, context)
-#     elif req == 'вопросы собеседования по сетям':
-#         net_question(update, context)
-#
-#     elif req == 'выйти':
-#         update.message.reply_text('Идём дальше', reply_markup=markup_ege)
-#     else:
-#         update.message.reply_text('Я тебя не понимаю. Напиши "команды", чтобы узнать список доступных функций.')
-#
-#
-# if __name__ == '__main__':
-#     main()
-
-
-# !/usr/bin/env python
-# pylint: disable=unused-argument, import-error
-# This program is dedicated to the public domain under the CC0 license.
-
-from typing import Dict
-
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
     Application,
@@ -83,11 +5,12 @@ from telegram.ext import (
     ContextTypes,
     ConversationHandler,
     MessageHandler,
-    filters,
-)
+    filters)
+import database_funcs
+from secret import TOKEN
+
 
 CHOOSING_PROFILE, TYPING_REPLY, TYPING_CHOICE, CHOOSING_DIRECTION, FIX_SUBJECT = range(5)
-
 reply_keyboard_profile = [['Имя', 'Курс'], ['Институт'], ['Закончить']]
 markup_profile = ReplyKeyboardMarkup(reply_keyboard_profile, one_time_keyboard=True)
 keyboard_go = [['Вперёд!']]
@@ -116,7 +39,7 @@ async def profile(update, context):
         return CHOOSING_DIRECTION
 
 
-async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def regular_choice(update, context):
     text = update.message.text.lower()
     context.user_data["choice"] = text
     keyboard_course = [['1', '2'], ['3', '4'], ['5', '6']]
@@ -133,7 +56,7 @@ async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     return TYPING_REPLY
 
 
-async def received_information(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def received_information(update, context):
     user_data = context.user_data
     text = update.message.text
     category = user_data["choice"]
@@ -176,7 +99,7 @@ async def fix_subject(update, context):
     return FIX_SUBJECT
 
 
-async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def done(update, context):
     user_data = context.user_data
     if "choice" in user_data:
         del user_data["choice"]
