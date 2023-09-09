60355e48493746812197bbb49c7e8b5cfa045ae4
Dazhy Khovalyg <hovalyg_d@mail.ru>
2023-09-09 15:54:24 +0300

    рабочая кнопа сообедник

--- have_a_break.py
+++ have_a_break.py
@@ -4,9 +4,10 @@ from queue import Queue
 
 
 
-def share_username():
+def share_username(status):
     lunch_person_id = database_funcs.get_a_lunch_person()[0]
-    database_funcs.swap_lunch_status(lunch_person_id)
+
+    database_funcs.swap_lunch_status(lunch_person_id, status)
     return lunch_person_id
 
 
@@ -14,13 +15,13 @@ def share_username():
 def search_for_lunch(user_id):
     database_funcs.add_user_to_lunch(user_id)
     status = database_funcs.get_lunch_status(user_id)
-    if len(database_funcs.get_a_lunch_person()) == 0:
+    if database_funcs.get_a_lunch_person() == None:
         database_funcs.swap_lunch_status(user_id, status)
-        while len(database_funcs.get_a_lunch_person()) == 1:
-            pass        
-    user2_id = share_username()
-    user2_name = database_funcs.get_profile(user2_id)
-    return user2_name
+        return ('wait',)
+    else:
+        user2_id = share_username(status)
+        user2_profile = database_funcs.get_profile(user2_id)
+        return user2_profile
     
 
 
